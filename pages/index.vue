f59f16cf126146249f09caac4b4fe123d210bccc
Vladimir Blaz <vladimir.blaz@outlook.com>
2023-11-21 16:22:51 -0400

    Get dogs pictures on load

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h1>PRUEBA</h1>
+    <img :src="dogResponse.message" />
+  </section>
+</template>
+
+<script setup lang="ts">
+  const { data: dogResponse } = await useFetch('https://dog.ceo/api/breeds/image/random');
+</script>
